730d2326541c8575f3c427f2ad26f7ace5af59a5
Ciapa <ciapa@lewd.wtf>
2024-03-30 17:37:21 +0100

    Use minimal logic to push to attic cache

---
+++ .github/push-derivations.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+OLDPATHS=$(cat /tmp/store-paths | jq -r '.[].path')
+NEWPATHS=$(nix path-info --all --json | jq -r '.[].path')
+
+PUSHPATHS=$(comm -13 <(printf '%s\n' "${OLDPATHS[@]}" | sort) <(printf '%s\n' "${NEWPATHS[@]}" | sort) | egrep -v '(.drv|.drv.chroot|.check|.lock)$')
+
+tries=0
+while IFS= read -r line; do
+    echo Pushing $line...
+    if attic push $CACHE $line ; then
+        tries=0
+    else
+        tries=$((tries+1))
+    fi
+
+    if [ $tries -eq 5 ]; then
+        echo "Repeated failure while pushing to attic cache!"
+        exit 1
+    fi
+done <<< $PUSHPATHS
+
