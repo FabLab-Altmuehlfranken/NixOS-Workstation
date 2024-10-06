b2f310c3ae283a9d910230c9bc09f1daf1fe7c2e
Markus <ciapa@lewd.wtf>
2024-10-06 17:59:52 +0000

    Remove obsolete sound.enable config option

--- configuration.nix
+++ configuration.nix
@@ -62,7 +62,6 @@
   #console.keyMap = "de";
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
