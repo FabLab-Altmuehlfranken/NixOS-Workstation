425aab9e3381e20ddb8c1381c9e234966bdb2ad6
Ecchibitionist <ciapa@lewd.wtf>
2025-05-11 21:25:58 +0200

    Add Nvidia GPU support to separate profile
    This is moved to a separate profile as mixing free and proprietary
    OpenGL implementations is highly discouraged

--- flake.nix
+++ flake.nix
@@ -44,5 +44,45 @@
         })
       ];
     };
+    nixosConfigurations.fablab-nvidia = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, ... }: {
+          system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
+          _module.args = { inherit inkstitch; };
+
+          systemd.tmpfiles.rules = [
+            "L+ /nix/nixPath - - - - ${pkgs.path}"
+          ];
+
+          nix = {
+            package = pkgs.nixVersions.stable;
+            extraOptions = ''
+              experimental-features = nix-command flakes
+            '';
+            registry.nixpkgs.flake = nixpkgs;
+            nixPath = [ "nixpkgs=/nix/nixPath" ];
+
+            settings = {
+              substituters = [
+                "https://cache.nixos.org"
+                # Prioritize local attic cache when building system locally
+                "https://attic.fablab-altmuehlfranken.de/fablab?priority=10"
+              ];
+
+              trusted-public-keys = [
+                "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+                "fablab:Tb/JDq91JCPGrMeK2FAzY3F6/ot3n5abG+14f521jr4="
+              ];
+            };
+          };
+
+          imports = [
+            ./configuration.nix
+            ./nvidia.nix
+          ];
+        })
+      ];
+    };
   };
 }
