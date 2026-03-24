94975df7af9e5d6de94d0226cacb3883a67ef542
danjujan <44864658+danjujan@users.noreply.github.com>
2026-03-24 19:24:09 +0100

    Set HP Color LaserJet as default printer

--- printers.nix
+++ printers.nix
@@ -8,25 +8,28 @@
   # for a WiFi printer
   services.avahi.openFirewall = true;
 
-  hardware.printers.ensurePrinters = [
-    {
-      description = "HP Color LaserJet CP5225dn";
-      deviceUri = "dnssd://HP%20Color%20LaserJet%20CP5225dn%20(CE4B57)._pdl-datastream._tcp.local/";
-      model = "drv:///sample.drv/generic.ppd";
-      name = "HP_Color_LaserJet_CP5225dn";
-      ppdOptions = {
-        PageSize = "A4";
-        Option1 = "True";
-      };
-    }
-    {
-      description = "Roland CAMM-1 GS-24";
-      deviceUri = "http://schneidplotter.local:631/printers/Roland_GS-24";
-      model = "raw";
-      name = "GS-24";
-      ppdOptions = {
-        PageSize = "A4";
-      };
-    }
-  ];
+  hardware.printers = {
+    ensureDefaultPrinter = "HP_Color_LaserJet_CP5225dn";
+    ensurePrinters = [
+      {
+        description = "HP Color LaserJet CP5225dn";
+        deviceUri = "dnssd://HP%20Color%20LaserJet%20CP5225dn%20(CE4B57)._pdl-datastream._tcp.local/";
+        model = "drv:///sample.drv/generic.ppd";
+        name = "HP_Color_LaserJet_CP5225dn";
+        ppdOptions = {
+          PageSize = "A4";
+          Option1 = "True";
+        };
+      }
+      {
+        description = "Roland CAMM-1 GS-24";
+        deviceUri = "http://schneidplotter.local:631/printers/Roland_GS-24";
+        model = "raw";
+        name = "GS-24";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+  }
 }
